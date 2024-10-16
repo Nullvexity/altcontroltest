93c5b6bf55a1d23dcffc8dc8bdea3491e6e4e3c5
Nullware <115350918+Nullvexity@users.noreply.github.com>
2024-10-16 21:28:32 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -9,7 +9,7 @@ workspace.Ignored.Drop.ChildAdded:Connect(function(drop)
             local cashText = cashPart.TextLabel.Text  -- Get the cash amount as a string
             local cleanedCash = string.gsub(cashText, "[^%d]", "") 
             local cashValue = tonumber(cleanedCash) or 0  -- Convert to a number
-            totalCash = totalCash + cashValue
+            moneydropped = moneydropped + cashValue
             print("Total Cash Dropped: $" .. totalCash)
         end
     end
